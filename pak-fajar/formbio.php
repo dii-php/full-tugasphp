1e1956f6799626fb5a86c4667e5f2c6a01d01ee7
Alediii <73929515+dii-php@users.noreply.github.com>
2025-09-25 17:47:21 +0800

    Update formbio.php

--- pak-fajar/formbio.php
+++ pak-fajar/formbio.php
@@ -109,7 +109,7 @@
 <div class="container">
 <?php
 // Koneksi ke database
-$koneksi = new mysqli("sql102.infinityfree.com", "if0_39908296", "pantoloan87", "if0_39908296_bio");
+$koneksi = new mysqli("gabole wlee", "gabole wlee", "apa lagi ini, gabole laa", "biodata_db");
 if ($koneksi->connect_error) {
     die("Koneksi gagal: " . $koneksi->connect_error);
 }
